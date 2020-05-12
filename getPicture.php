6656e1e44cf9038111b5ce4832348abc67adbb5d
foch <fu.967@qq.com>
2020-05-12 16:18:26 +0800

    修改端口模式

--- getPicture.php
+++ getPicture.php
@@ -9,7 +9,7 @@ if (empty($params)) {
   exit();
 }
 $params = strtolower($params);
-$serve = 'mysql:host=cdb-qq35tjif.gz.tencentcdb.com:10124;dbname=monet;charset=utf8';
+$serve = 'mysql:host=cdb-qq35tjif.gz.tencentcdb.com;port=3306;dbname=monet;charset=utf8';
 $username = 'root';
 $password = 'fuiii967###';
 // phpinfo()
